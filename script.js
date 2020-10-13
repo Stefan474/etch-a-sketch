d71e6c81f068d1d41f8858f8a4c4f9876d822226
Stefan <fanste474@gmail.com>
2020-10-13 18:54:50 +0200

    Final push, fixed the firefox display bug and added the 'rainbow' functionality to the eventlistener. Should be fully functioning now!

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var container = document.getElementById("container");
 
+//initializing a default grid, and running the event handler 
+makeGrid(20,20);
+test();
+
+// this function clears the board from any changes
+document.getElementById("clear").addEventListener('click', () => {
+    var clearTarget = document.getElementsByClassName("divs");
+    for(i = 0; i < clearTarget.length; i++){
+        clearTarget[i].style.backgroundColor="bisque";
+    }
+})
+
+/* a grid generator, calculates cell size by
+dividing the container size with the number of rows and collumns*/
 function makeGrid(rows, columns){
-    let cellH = container.clientHeight/rows;
+    let cellH =  container.clientHeight/rows;
     
     let cellW = container.clientWidth/columns;
+
     
 
     for(let i = 0; i < rows; i++){
@@ -17,10 +32,37 @@ function makeGrid(rows, columns){
             container.appendChild(newCell);
         }
     }
+    
 }
 
+//event landler for the make a grid button
 document.getElementById("test").addEventListener('click' , () => {
     var x = document.getElementById("gridInfo").value;
+    if (x>100){
+        alert("Cell height/width is bigger than 100. Pick a number smaller than 100.")
+        return;}
     container.innerHTML = '';
     makeGrid(x,x);
+    test();
 });
+
+//used to roll rgb colors in the main function 
+function getRandomColor(){
+    color = Math.ceil(Math.random() * 256);
+    return color;
+}
+
+// beautifully named function that handles the main etch-a-sketch event
+function test(){
+    var generatedDivs = document.getElementsByClassName("divs");
+    for(i = 0; i < generatedDivs.length; i++){
+        generatedDivs[i].addEventListener('mouseover', e => {
+            if(document.getElementById("rainbow").checked === true){
+                e.target.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}`
+            }else{
+                e.target.style.backgroundColor = "black";
+            }
+    }
+
+ )}
+};
